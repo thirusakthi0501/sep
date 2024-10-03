38b61643bcf7480247f0314efcaf040dad9b450e
thirusakthi0501 <104378608+thirusakthi0501@users.noreply.github.com>
2024-10-03 14:50:16 +0530

    Create diifsum.c

---
+++ diifsum.c
@@ -0,0 +1,42 @@
+/*
+The function accepts two integers n, m as arguments Find the sum of all numbers in range from 1 to m(both inclusive) that are not divisible by n. Return difference between sum of integers not divisible by n with sum of numbers divisible by n.
+
+Assumption:
+
+n>0 and m>0
+Sum lies between integral range
+Example
+
+Input
+n:4
+m:20
+Output
+90
+
+Explanation
+
+Sum of numbers divisible by 4 are 4 + 8 + 12 + 16 + 20 = 60
+Sum of numbers not divisible by 4 are 1 +2 + 3 + 5 + 6 + 7 + 9 + 10 + 11 + 13 + 14 + 15 + 17 + 18 + 19 = 150
+Difference 150 – 60 = 90
+Sample Input
+n:3
+m:10
+Sample Output
+19
+*/
+#include <stdio.h>
+#include<string.h>
+#include<ctype.h>
+
+int main() {
+   int q,w,n,m;
+   scanf("%d %d",&q,&w);
+   for(int e=1;e<=w;e++){
+       if(e%q==0)
+       n+=e;
+       else
+       m+=e;
+   }
+   printf("%d ",m-n);
+}
+
