8ec5488bedf88a0fb77dafced0820df59845e17f
thirusakthi0501 <104378608+thirusakthi0501@users.noreply.github.com>
2024-10-06 14:17:16 +0530

    Create gcd \hcf.c

---
+++ top 50/gcd \hcf.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main(){
+    int a,s;
+    scanf("%d %d",&a,&s);
+    int r=((a<s)?a:s);
+    while(r>0){
+        if(a%r==0 && s%r==0)
+        break;
+        r--;
+    }
+    printf("%d ",r);
+}
